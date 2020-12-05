6a575c22221bc3600cdffa9b0d3cbc75612b5e63
ParkSangSun1 <appleasz1028102@gmail.com>
2020-12-05 23:14:23 +0900

    setting ui 변경, 기능추가

--- app/src/main/java/com/example/tictactoe_java/settingActivity.java
+++ app/src/main/java/com/example/tictactoe_java/settingActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class settingActivity extends AppCompatActivity {
 
-    private ImageView go_main;
+    private Button go_main;
     EditText player1;
     EditText player2;
     @Override
@@ -19,11 +21,15 @@ public class settingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setting);
 
 
+
+
         go_main = findViewById(R.id.go_main);
 
         go_main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
 
                 EditText player_name1 = (EditText) findViewById(R.id.player_name1);
                 EditText player_name2 = (EditText) findViewById(R.id.player_name2);
@@ -33,12 +39,18 @@ public class settingActivity extends AppCompatActivity {
                 String name2 = player_name2.getText().toString();
                 String settingscore1 = settingscore.getText().toString();
 
-                Intent intent= new Intent(settingActivity.this, MainActivity.class);
-                intent.putExtra("입력한 이름1",name1);
-                intent.putExtra("입력한 이름2",name2);
-                intent.putExtra("설정 스코어",settingscore1);
 
-                startActivity(intent);
+                if (settingscore.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(),"점수를 설정하세요!",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Intent intent = new Intent(settingActivity.this, MainActivity.class);
+                    intent.putExtra("입력한 이름1", name1);
+                    intent.putExtra("입력한 이름2", name2);
+                    intent.putExtra("설정 스코어", settingscore1);
+
+                    startActivity(intent);
+                }
             }
         });
 
