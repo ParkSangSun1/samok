ce9d7e0fae0d577ad6e901321bc249a0e3c06bc1
ParkSangSun1 <appleasz1028102@gmail.com>
2020-12-07 16:21:07 +0900

    main ui변경

--- app/src/main/java/com/example/tictactoe_java/LodingActivity.kt
+++ app/src/main/java/com/example/tictactoe_java/LodingActivity.kt
@@ -28,7 +28,7 @@ class LodingActivity : AppCompatActivity() {
         @Suppress("DEPRECATION")
         Handler().postDelayed(
                 {
-                    startActivity(Intent(this@LodingActivity, settingActivity::class.java))
+                    startActivity(Intent(this@LodingActivity, new_setting::class.java))
                     finish()
                 },
                 2500
